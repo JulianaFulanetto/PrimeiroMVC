9515bf4dba9fb87db402c0102b685e14003fa5b5
Juliana <julianafulanetto.senai@gmail.com>
2024-08-27 14:27:39 -0300

    Finalizado o exemplo de MVC com lista de alunos

--- PrimeiroMVC/Controllers/AlunoController.cs
+++ PrimeiroMVC/Controllers/AlunoController.cs
@@ -7,11 +7,11 @@ namespace PrimeiroMVC.Controllers
     {
         public IActionResult Index()
         {
-            Aluno a1 = new Aluno(1, "Robsinho da Silva", "rosinho.silva@sp.senai.br", 12345);
-            Aluno a2 = new Aluno(1, "Cleiton Ronivaldo", "cleiton.roni@sp.senai.br", 56321);
-            Aluno a3 = new Aluno(1, "Jeremias Ratinni", "jeremias.ratin@sp.senai.br", 98643);
-            Aluno a4 = new Aluno(1, "Sorocaba Almeida", "sorocaba.almeida@sp.senai.br", 95374);
-            Aluno a5 = new Aluno(1, "Fernandin Leme", "@fernandun.leme@sp.senai.br", 99872);
+            Aluno a1 = new Aluno(1, "Robsinho da Silva", "robsinho.silva@sp.senai.br", 12345);
+            Aluno a2 = new Aluno(2, "Cleiton Ronivaldo", "cleiton.roni@sp.senai.br", 56321);
+            Aluno a3 = new Aluno(3, "Jeremias Ratinni", "jeremias.ratin@sp.senai.br", 98643);
+            Aluno a4 = new Aluno(4, "Sorocaba Almeida", "sorocaba.alm@sp.senai.br", 95374);
+            Aluno a5 = new Aluno(5, "Fernandin Leme", "fernandin.leme@sp.senai.br", 99872);
 
             //criar uma lista de alunos
             List<Aluno> listaAluno = new List<Aluno>();
